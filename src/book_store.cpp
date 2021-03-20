baa8f602a0b37c1e89dc097fce7750539109c46d
Roman Browin <roman.browin@yandex.ru>
2021-03-20 19:06:21 +0400

    add homework h01

--- src/book_store.cpp
+++ src/book_store.cpp
@@ -7,6 +7,19 @@
 ResizeStorageStatus resize_storage(Book *&storage, int size, int new_capacity) {
   // здесь мог бы быть ваш разносторонний и многогранный код ...
   // Tip 1: проведите валидацию аргументов функции
+  if(storage == nullptr){
+      return ResizeStorageStatus::NULL_STORAGE;
+  }
+  if(new_capacity <= size){
+      return ResizeStorageStatus::INSUFFICIENT_CAPACITY;
+  }
+  if(size < 0){
+      return ResizeStorageStatus::NEGATIVE_SIZE;
+  }
+  Book *new_storage = new Book[new_capacity]{};
+  std::copy(storage, storage + size, new_storage);
+  delete[] storage;
+  storage = new_storage;
   // Tip 2: не забудьте высвободить ранее выделенную память под хранилище
   return ResizeStorageStatus::SUCCESS;
 }
@@ -19,11 +32,19 @@ BookStore::BookStore(const std::string &name) : name_{name} {
   }
 
   // здесь мог бы быть ваш сотрясающий землю и выделяющий память код ...
+  name_ = name;
+  storage_ = new Book[kInitStorageCapacity]{};
+  storage_size_ = 0;
+  storage_capacity_ = kInitStorageCapacity;
 }
 
 // 3. реализуйте деструктор ...
 BookStore::~BookStore() {
   // здесь мог бы быть ваш высвобождающий разум от негатива код ...
+  delete[] storage_;
+  storage_ = nullptr;
+  storage_size_ = 0;
+  storage_capacity_ = 0;
   // Tip 1: я свободен ..., словно память в куче: не забудьте обнулить указатель
 }
 
@@ -31,9 +52,14 @@ BookStore::~BookStore() {
 void BookStore::AddBook(const Book &book) {
   if (storage_size_ == storage_capacity_) {
     // здесь мог бы быть ваш умопомрачительный код ...
-    // Tip 1: используйте функцию resize_storage_internal, задав новый размер хранилища
+    if (resize_storage_internal(storage_capacity_ + kCapacityCoefficient) != ResizeStorageStatus::SUCCESS){
+        return;
+    }
+    // Tip 1: используйте функцию resize_storage_internal, задав новый размер хранилищ
     // Tip 2: не забудьте обработать статус вызова функции
   }
+    storage_[storage_size_] = book;
+    storage_size_ += 1;
   // Tip 3: не забудьте добавить книгу в наше бездонное хранилище ...
 }
 
